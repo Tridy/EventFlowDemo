cb47d2f4688c39817f862a5fdcce1182c7ebd410
Genadij Trimailov <131436682+genadij-trimailov-flextools@users.noreply.github.com>
2023-10-26 10:14:36 +0200

    initial

---
+++ src/MyConsole/MySuperDuperEventSource.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace MyConsole
+{
+    [EventSource(Name = "MySuperDuperEventSource")]
+    public class MySuperDuperEventSource : EventSource
+    {
+        public static MySuperDuperEventSource Log = new();
+
+        [Event(1001, Message = "Application Starting")]
+        public void ApplicationStarting()
+        {
+            if (IsEnabled())
+            {
+                WriteEvent(1001);
+            }
+        }
+
+        [Event(2001, Message = "User Login: {0}")]
+        public void UserLogin(string userName)
+        {
+            if (IsEnabled())
+            {
+                WriteEvent(2001, userName);
+            }
+        }
+
+        [Event(2003, Message = "Element processed: {0}")]
+        public void ElementProcessed(string elementName)
+        {
+            if (IsEnabled())
+            {
+                WriteEvent(2003, elementName);
+            }
+        }
+
+        [Event(9001, Message = "Application Ending")]
+        public void ApplicationEnding()
+        {
+            if (IsEnabled())
+            {
+                WriteEvent(9001);
+            }
+        }
+    }
+}
---
+++ src/MyConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Diagnostics.EventFlow;
+
+namespace MyConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var pipeline = DiagnosticPipelineFactory.CreatePipeline("eventFlowConfig.json"))
+            {
+                MySuperDuperEventSource.Log.ApplicationStarting();
+                MySuperDuperEventSource.Log.UserLogin("Some One");
+
+                DoSomeWork();
+
+                MySuperDuperEventSource.Log.ApplicationEnding();
+            }
+        }
+
+        private static void DoSomeWork()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                MySuperDuperEventSource.Log.ElementProcessed("Element " + i.ToString());
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
